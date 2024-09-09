35616cb455c312fe7c3499efcaf73676fd3eefba
kili-manu <killymanu071@gmail.com>
2024-09-09 17:41:51 -0500

    convert to pdf

---
+++ variables.py
@@ -0,0 +1,41 @@
+       #mutiplication
+print("5 multiplied by 6")
+x=5
+y=6
+z= x * y
+print("The answer to your query is " , z)
+
+       #divide
+print("8 divided by 3")
+x=8
+y=3
+z=x/y
+print("The answer to your query is" , z)
+
+         #//
+print("8 divided by 3")
+x=8
+y=3
+z=x//y
+print("The answer to your query is" ,z)
+
+        #modulo
+print("8 modulo 3")
+x=8
+y=3
+z=x%y
+print("x%y The answer to your query is" ,z)
+
+        #addition
+print("4200000 add 23090")
+x=4200000
+y=23090
+t = x + y
+print("The answer to your query is", t)
+
+         #subtract
+print("4200000 subtract 7633")
+a=4200000
+b=7633
+d= a-b
+print("The answer to your query is" , d)
